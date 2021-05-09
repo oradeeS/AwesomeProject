0dfa805047887e65db37e54c7bb248b1e22c4558
oradeeS <74482490+oradeeS@users.noreply.github.com>
2021-05-09 16:55:06 +0700

    props&flatlist

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 
 
-import Bmi from './screens/Bmi';
+import Network from './components/Network';
 export default function App() {
   return (
-    <Bmi />
+    <Network />
   );
 }
 
